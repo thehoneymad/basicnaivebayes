f157858c0faed049a9348d33ab044089f474f060
Tanzim Saqib <tanzimsaqib@live.com>
2015-02-05 01:05:36 +0600

    Fixed VS2013 project to accommodate the current folder structure

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
-var projRequire=require('./projRequire.js');
+var projRequire = require('./projRequire.js');
 
 var utils = projRequire("lib::Utils");
 var NaiveBayes = projRequire("lib::NaiveBayes");
 
-(function (title) {
+(function(title) {
     var classifier = new NaiveBayes();
-    
-    classifier.learn(['I love Windows Phone, that thing is amazing', 
-                    'Awesome this is incredibly perfect, great!!'
+
+    classifier.learn([
+        'I love Windows Phone, that thing is amazing',
+        'Awesome this is incredibly perfect, great!!'
     ], 'positive');
-    
+
     classifier.learn('Bad, Laggy thing. Damn. Crap!!', 'negative');
     var category = classifier.classify('Its a real bad thing to say');
-    
+
     utils.assert('negative' === category, title);
 })('Basic 3-liner test should return negative');
 
-(function (title) {
+(function(title) {
     var classifier = new NaiveBayes();
-    
-    classifier.learn(['Nut rage: Korean Air boss\'s daughter treated crew like slaves', 
-                    'Top court upholds Japanese man\'s death sentence for stabbing rampage',
-                    'Al-Jazeera journalist\'s colleagues still held in Egypt'
+
+    classifier.learn([
+        'Nut rage: Korean Air boss\'s daughter treated crew like slaves',
+        'Top court upholds Japanese man\'s death sentence for stabbing rampage',
+        'Al-Jazeera journalist\'s colleagues still held in Egypt'
     ], 'negative');
 
-    classifier.learn(['Super Bowl halftime show', 
-                    'Joanne Peh talks baby bump and weird cravings',
+    classifier.learn([
+        'Super Bowl halftime show',
+        'Joanne Peh talks baby bump and weird cravings'
     ], 'positive');
-   
+
     var category = classifier.classify('Al-Jazeera makes news of Joanne Peh\'s daughter');
-    
+
     utils.assert('positive' === category, title);
 })('Basic 5-liner news titles should return positive');
 
